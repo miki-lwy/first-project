f225c8a5c7f6c5bfb936581f5d64a0c14f1182bf
miki-lwy <miki.lwy@gmail.com>
2018-08-18 22:14:15 +0800

    used person_database module

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 import http.server
 import json
 import urllib.request
+import person_database
 
+db = person_database.PersonDatabase()
 
-class Person:
-    def __init__(self, nickname, gender, name):
-        self.nickname = nickname
-        self.gender = gender
-        self.name = name
-
-
-
-fruit_database = [
-    "banana",
-    "apple",
-    "pear"
-]
-
-person_database = []
 
 # initialization
 with open('example_file.csv', 'r', newline='') as filereader:
@@ -25,10 +12,8 @@ with open('example_file.csv', 'r', newline='') as filereader:
     for row in filereader:
         row = row.strip()
         row_list = row.split(',')
-        new_person = Person(row_list[0], row_list[1], row_list[2])
-        person_database.append(new_person)
-
-
+        new_person = person_database.Person(row_list[0], row_list[1], row_list[2])
+        db.create(new_person)
 
 
 
@@ -44,11 +29,11 @@ class MyHandlerForHTTP(http.server.BaseHTTPRequestHandler):
         for element in parsed_body:
             parsed_element = element.split('=')
             person_dict[parsed_element[0]] = parsed_element[1]
-        new_person = Person(person_dict['nickname'], person_dict['gender'], person_dict['name'])
-        person_database.append(new_person)
+        new_person = person_database.Person(person_dict['nickname'], person_dict['gender'], person_dict['name'])
+        db.create(new_person)
         with open('example_file.csv', 'w') as filewriter:
             filewriter.write('nickname,gender,name \n')
-            for person in person_database:
+            for person in db.list:
                 filewriter.write(person.nickname + ',' + person.gender + ',' + person.name + '\n')
 
     def do_PUT(self):
@@ -58,21 +43,13 @@ class MyHandlerForHTTP(http.server.BaseHTTPRequestHandler):
         content_len = int(self.headers.get('content-length', 0))
         put_body = self.rfile.read(content_len)
         json_body = json.loads(put_body)
-        amend_person = Person(json_body['nickname'], json_body['gender'], json_body['name'])
-        found = False
-        for person in person_database:
-            nickname_to_find = self.extract_nickname()
-            if person.nickname == nickname_to_find:
-                found = True
-                person_database.remove(person)
-                person_database.append(amend_person)
-                with open('example_file.csv', 'w') as filewriter:
-                    filewriter.write('nickname,gender,name \n')
-                    for person in person_database:
-                        filewriter.write(person.nickname + ',' + person.gender + ',' + person.name + '\n')
+        amend_person = person_database.Person(json_body['nickname'], json_body['gender'], json_body['name'])
+        db.update(amend_person)
+        with open('example_file.csv', 'w') as filewriter:
+            filewriter.write('nickname,gender,name \n')
+            for person in db.list:
+                filewriter.write(person.nickname + ',' + person.gender + ',' + person.name + '\n')
 
-        if not found:
-            self.wfile.write(bytes('<p> Cannot find this person </p>', 'UTF-8'))
 
     def extract_nickname(self):
         return self.path.replace('/friends/', '')
@@ -81,20 +58,12 @@ class MyHandlerForHTTP(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
-        found = False
-        for person in person_database:
-            nickname_to_find = self.extract_nickname()
-            if person.nickname == nickname_to_find:
-                found = True
-                person_database.remove(person)
-                with open('example_file.csv', 'w') as filewriter:
-                    filewriter.write('nickname,gender,name \n')
-                    for person in person_database:
-                        filewriter.write(person.nickname + ',' + person.gender + ',' + person.name + '\n')
-
-        if not found:
-            self.wfile.write(bytes('<p> Cannot find this person </p>', 'UTF-8'))
-
+        nickname_to_find = self.extract_nickname()
+        db.delete(nickname_to_find)
+        with open('example_file.csv', 'w') as filewriter:
+            filewriter.write('nickname,gender,name \n')
+            for person in db.list:
+                filewriter.write(person.nickname + ',' + person.gender + ',' + person.name + '\n')
 
 
     def do_GET(self):
@@ -108,16 +77,11 @@ class MyHandlerForHTTP(http.server.BaseHTTPRequestHandler):
         } \
         </style></head><body>', 'UTF-8'))
 
-        if self.path == '/fruits':
-            self.wfile.write(bytes('<h1> Here are your fruits: </h1>', 'UTF-8'))
-            for fruit in fruit_database:
-                self.wfile.write(bytes('<h1>name: </h1>' + fruit, 'UTF-8'))
-
         if self.path == '/friends':
             self.wfile.write(bytes('<h1> Here are your friends: </h1>', 'UTF-8'))
             self.wfile.write(bytes('<table style="width:100%">', 'UTF-8'))
             self.wfile.write(bytes('<tr> <th>Nickname</th> <th>Name</th> <th>Gender</th></tr>', 'UTF-8'))
-            for person in person_database:
+            for person in db.list:
                 self.wfile.write(bytes('<tr>'
                                        '<td>'
                                        '<a href="http://localhost:8088/friends/' + person.nickname + '">' + person.nickname + '</a>'
@@ -140,14 +104,12 @@ class MyHandlerForHTTP(http.server.BaseHTTPRequestHandler):
 
         if self.path.startswith('/friends/'):
             nickname_to_find = self.extract_nickname()
-            found = False
-            for person in person_database:
-                if person.nickname == nickname_to_find:
-                    found = True
-                    self.wfile.write(bytes('<h1> ' + person.nickname + '\'s details: </h1>', 'UTF-8'))
-                    self.wfile.write(bytes('<dl><dt>Gender: </dt> <dd>' + person.gender + '</dd>', 'UTF-8'))
-                    self.wfile.write(bytes('<dt>Name: </dt> <dd>' + person.name + '</dd></dl>', 'UTF-8'))
-            if not found:
+            person = db.find(nickname_to_find)
+            if person is not None:
+                self.wfile.write(bytes('<h1> ' + person.nickname + '\'s details: </h1>', 'UTF-8'))
+                self.wfile.write(bytes('<dl><dt>Gender: </dt> <dd>' + person.gender + '</dd>', 'UTF-8'))
+                self.wfile.write(bytes('<dt>Name: </dt> <dd>' + person.name + '</dd></dl>', 'UTF-8'))
+            else:
                 self.wfile.write(bytes('<p> I don\'t know this person </p>', 'UTF-8'))
 
         self.wfile.write(bytes('</body></html>', 'UTF-8'))
